ecdc025c6643d8a0829ba19fdb20e53eeb11a4a0
Thibault Falise <thibault.falise@gdfsueztrading.com>
2015-09-09 15:52:57 +0200

    Updated tests to run in VS2013 / VS2015

--- source/OctoPack.Tests/Integration/BuildFixture.cs
+++ source/OctoPack.Tests/Integration/BuildFixture.cs
@@ -6,6 +6,9 @@ using NUnit.Framework;
 using NuGet;
 using OctoPack.Tasks;
 using OctoPack.Tasks.Util;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OctoPack.Tests.Integration
 {
@@ -33,13 +36,34 @@ namespace OctoPack.Tests.Integration
 
         protected static void MsBuild(string commandLineArguments)
         {
-            MsBuild(commandLineArguments, null );
+            MsBuild(commandLineArguments, null);
+        }
+
+        private static IEnumerable<string> VsMsBuildPaths()
+        {
+            var tryVersions = new[] { "14.0", "12.0" };// 11?
+            foreach (var version in tryVersions)
+            {
+                var location = ToolLocationHelper.GetPathToBuildToolsFile(
+               "msbuild.exe", version,
+               DotNetFrameworkArchitecture.Bitness64);
+                if (location != null)
+                    yield return location;
+            }
+        }
+
+        private static string FrameworkMsbuild()
+        {
+            var netFx = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
+            var msBuild = Path.Combine(netFx, "msbuild.exe");
+            return msBuild;
         }
 
         protected static void MsBuild(string commandLineArguments, Action<string> outputValidator)
         {
-            var netFx = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory();
-            var msBuild = Path.Combine(netFx, "msbuild.exe");
+            var vsMsBuild = VsMsBuildPaths().FirstOrDefault(path => File.Exists(path));
+            var msBuild = vsMsBuild ?? FrameworkMsbuild();
+
             if (!File.Exists(msBuild))
             {
                 Assert.Fail("Could not find MSBuild at: " + msBuild);
